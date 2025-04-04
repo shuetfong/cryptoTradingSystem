54da519c811b9f1540cd892320b11d67ff62d0e2
shuetfong <shuetfongcheang@gmail.com>
2025-04-04 16:02:38 +0800

    initial commit

---
+++ src/test/java/com/example/cryptotradingsystem/CryptoTradingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptotradingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTradingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
